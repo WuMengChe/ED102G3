9ea35920621d4a1468464351866d3783c2a9f410
Jing-siao <vicky820715@gmail.com>
2020-08-25 21:32:36 +0800

    上傳後台

--- js/post_card/post_change.js
+++ js/post_card/post_change.js
@@ -4,21 +4,31 @@ $(function () {
     $('#frontStyle1').show();
     $('#frontStyle2').hide();
     $('#frontStyle3').hide();
+    $(this).parent().siblings().removeClass("active");
+    $(this).parent().addClass("active");
   });
   $('.f_img_s2').click(function () {
     $('#frontStyle2').show();
     $('#frontStyle1').hide();
     $('#frontStyle3').hide();
+    $(this).parent().siblings().removeClass("active");
+    $(this).parent().addClass("active");
   });
   $('.f_img_s3').click(function () {
     $('#frontStyle3').show();
     $('#frontStyle2').hide();
     $('#frontStyle1').hide();
+    $(this).parent().siblings().removeClass("active");
+    $(this).parent().addClass("active");
   });
   // 換正面外框
   $(".outline_style div img").click(function () {
     var N = $(this).attr('id').substr(7);
     $('#mainImg').attr('src', './img/post_card/outline' + N + '.png');
+    $(this).parent().siblings().removeClass("active");
+    $(this).parent().addClass("active");
+
+
   });
   $(".outline_style div p").click(function () {
     var N = $(".outline_style div img").attr('id').substr(7);
@@ -27,7 +37,7 @@ $(function () {
   //桌機換格式的頁籤
   let deskLine = $(".post_desk .style_all_outline");
   let deskFront = $(".post_desk .front_style");
-  let deskBack = $(".post_desk .change_style .back_style");
+
 
   let deskStamps = $(".post_desk .change_style .style_all_stamps");
   let deskPostmarks = $(".post_desk .change_style .style_all_postmarks");
@@ -50,10 +60,12 @@ $(function () {
     deskStamps.show();
     deskPostmarks.hide();
 
+
   });
   $(".mark #backPostmarks").click(function () {
     deskStamps.hide();
     deskPostmarks.show();
+
 
   });
   //換郵票
@@ -61,12 +73,16 @@ $(function () {
     var N = $(this).attr('id').substr(5);
     $('#mainStamp').show();
     $('#mainStamp').attr('src', './img/post_card/stamp' + N + '.png');
+    $(this).parent().siblings().removeClass("active");
+    $(this).parent().addClass("active");
   });
   //換郵戳
   $(".postmarks_style div img").click(function () {
     var N = $(this).attr('id').substr(8);
     $('#postmark').show();
     $('#postmark').attr('src', './img/post_card/postmark' + N + '.png');
+    $(this).parent().siblings().removeClass("active");
+    $(this).parent().addClass("active");
   });
   //手機板加col-3 class
   $('#phoneFrontAllStyle .front_all_style .front_style .style').addClass('col-3');
