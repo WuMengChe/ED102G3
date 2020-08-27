74761622e7ff8078b94daebfbdf27b8a421a80a5
Jing-siao <vicky820715@gmail.com>
2020-08-27 18:54:39 +0800

    更新明信片

--- js/post_card/post_canvas.js
+++ js/post_card/post_canvas.js
@@ -2,17 +2,17 @@ document.getElementById('toBack').onclick = function doCapture() {
 
   window.scrollTo(0, 0);
   html2canvas(document.getElementById("frontStyle3")).then(function (canvas) {
-    // console.log(canvas.toDataURL("image/jpeg", 0.9));
-    var ajaxFront = new XMLHttpRequest();
-    ajaxFront.open("POST", "post_save.php", true);
-    ajaxFront.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    ajaxFront.send("img=" + canvas.toDataURL("image/jpeg", 0.9));
+    console.log(canvas.toDataURL("image/jpeg", 0.9));
+    // var ajaxFront = new XMLHttpRequest();
+    // ajaxFront.open("POST", "post_save.php", true);
+    // ajaxFront.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    // ajaxFront.send("img=" + canvas.toDataURL("image/jpeg", 0.9));
 
-    ajaxFront.onreadystatechange = function () {
-      if (this.readyState == 4 && this == 200) {
-        console.log(this.responseText);
-      }
-    };
+    // ajaxFront.onreadystatechange = function () {
+    //   if (this.readyState == 4 && this == 200) {
+    //     console.log(this.responseText);
+    //   }
+    // };
   });
 
 }
--- js/post_card/post_imgUp.js
+++ js/post_card/post_imgUp.js
@@ -29,8 +29,9 @@ function doFirst() {
   //拖曳郵戳
   $("#postmark").draggable();
 
-  // let label = image.getElementsByTagName("label");
-  // label.addEventListener("change", function () {
+  // let input = image.getElementsByTagName("label");
+  // label.addEventListener("change", function (e) {
+  //   let file = e.target.files[0];
   //   let readFile = new FileReader();
   //   readFile.readAsDataURL(file);
   //   readFile.addEventListener('load', function () {
@@ -40,43 +41,39 @@ function doFirst() {
   //     image.style.backgroundPosition = 'center';
   //   });
   // });
-  $("input[type='file']").change(function () {
-    var input = $("input[type='file']")[0];
-    if (input.files && input.files[0]) {
+  // $("input[type='file']").change(function () {
+  //   var input = $("input[type='file']")[0];
+  //   if (input.files && input.files[0]) {
 
-      reader.onload = function (e) {
-        e.preventDefault();
-        let readFile = new FileReader();
-        readFile.readAsDataURL(file);
-        let image = document.getElementById('frontStyle1');
-        image.style.background = `url(${readFile.result})`;
-        image.style.backgroundSize = 'cover';
-        image.style.backgroundRepeat = 'no-repeat';
-        image.style.backgroundPosition = 'center';
+  //     reader.onload = function (e) {
+  //       e.preventDefault();
+  //       let readFile = new FileReader();
+  //       readFile.readAsDataURL(file);
+  //       let image = document.getElementById('frontStyle1');
+  //       image.style.background = `url(${readFile.result})`;
+  //       image.style.backgroundSize = 'cover';
+  //       image.style.backgroundRepeat = 'no-repeat';
+  //       image.style.backgroundPosition = 'center';
 
-      }
-      reader.readAsDataURL(input.files[0]);
-    }
+  //     }
+  //     reader.readAsDataURL(input.files[0]);
+  //   }
 
-  });
+  // });
 }
 
 window.addEventListener('load', doFirst);
 
 
+// window.addEventListener("load", function () {
+//   document.getElementById("mem_idno_image").onchange = function (e) {
+//     let file = e.target.files[0];
+//     let reader = new FileReader();
 
-
-// $(function () {
-//   $("input[type='file']").change(function () {
-//     var input = $("input[type='file']")[0];
-//     if (input.files && input.files[0]) {
-//       var reader = new FileReader();
-//       reader.onload = function (e) {
-//         image.style.background = `url(${readFile.result})`;
-
-//       }
-//       reader.readAsDataURL(input.files[0]);
+//     reader.onload = function () {
+//       document.getElementById("imgPreciew").src = reader.result;
 //     }
 
-//   });
+//     reader.readAsDataURL(file);
+//   }
 // });
