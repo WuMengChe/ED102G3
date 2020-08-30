cf7d1648db721513f186cbd760eac51f626ce16f
Lin <linlin.81992@gmail.com>
2020-08-30 14:01:00 +0800

    select --- not yet

--- js/forum/forum_form.js
+++ js/forum/forum_form.js
@@ -1,11 +1,37 @@
-//下拉選單
-$('.dropdown-el').click(function (e) {
-  e.preventDefault();
-  e.stopPropagation();
-  $(this).toggleClass('expanded');
-  $('#' + $(e.target).attr('for')).prop('checked', true);
-});
-$(document).click(function () {
-  $('.dropdown-el').removeClass('expanded');
-});
+new Vue({
+  el: "#forum_form",
+  data() {
+    return {
+      isOpen: false,
+      isShareOpen: false
+    }
 
+  },
+  computed: {
+
+  },
+  methods: {
+    typeDropdown() {
+      if (this.isOpen) {
+        this.isOpen = false;
+      } else {
+        this.isOpen = true
+      }
+    },
+    selectType(type) {
+      this.type = type
+      this.typeDropdown()
+    },
+    shareDropdown() {
+      if (this.isShareOpen) {
+        this.isShareOpen = false;
+      } else {
+        this.isShareOpen = true
+      }
+    },
+    selectQU(type) {
+      this.type = type
+      this.shareDropdown()
+    }
+  },
+});
