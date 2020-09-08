232ce4e2c55063439f38a145a8c72812bcfde4cd
Jing-siao <vicky820715@gmail.com>
2020-09-08 16:08:48 +0800

    明信片改黨名

--- js/post_card/post_vue.js
+++ js/post_card/post_vue.js
@@ -22,14 +22,14 @@ Vue.component('outline-style', {
   template: `<div class="style_all_outline">
             <div class="outline_style row style_list">
               <div class="style">
-                <img src="./img/post_card/outline0.png" alt="無邊框" id="outline0">
+                <img src="./img/post_card/f_outline0.png" alt="無邊框" id="outline0">
                 <p class="p_small">無邊框</p>
               </div>
               <div class="style">
-                <img src="./img/post_card/outline1.png" alt="紅藍邊框" id="outline1">
+                <img src="./img/post_card/f_outline1.png" alt="紅藍邊框" id="outline1">
               </div>
               <div class="style">
-                <img src="./img/post_card/outline2.png" alt="黃藍邊框" id="outline2">
+                <img src="./img/post_card/f_outline2.png" alt="黃藍邊框" id="outline2">
               </div>
             </div>
             <div class="style_outline">
@@ -42,13 +42,15 @@ Vue.component('stamp-style', {
   template: `<div class="style_all_stamps">
               <div class="stamps_style row style_list">
                 <div class="style">
-                  <img src="./img/post_card/stamp1.png" alt="郵票1" id="stamp1">
+                  <img src="./img/post_card/b_stamp1.png" alt="郵票1" id="stamp1">
                 </div>
                 <div class="style">
-                  <img src="./img/post_card/stamp2.png" alt="郵票2" id="stamp2">
+                  <img src="./img/post_card/b_stamp2.png" alt="郵票2" id="stamp2">
                 </div>
                 <div class="style">
-                  <img src="./img/post_card/stamp3.png" alt="郵票3" id="stamp3">
+                  < img src = "./img/post_card/b_stamp3.png"
+                  alt = "郵票3"
+                  id = "stamp3" >
                 </div>
               </div>
             </div>`,
@@ -57,13 +59,19 @@ Vue.component('postmark-style', {
   template: `<div class="style_all_postmarks">
               <div class="postmarks_style row style_list">
                 <div class="style">
-                  <img src="./img/post_card/postmark1.png" alt="郵戳1" id="postmark1">
+                  < img src = "./img/post_card/b_postmark1.png"
+                  alt = "郵戳1"
+                  id = "postmark1" >
                 </div>
                 <div class="style">
-                  <img src="./img/post_card/postmark2.png" alt="郵戳2" id="postmark2">
+                  < img src = "./img/post_card/b_postmark2.png"
+                  alt = "郵戳2"
+                  id = "postmark2" >
                 </div>
                 <div class="style">
-                  <img src="./img/post_card/postmark3.png" alt="郵戳3" id="postmark3">
+                  < img src = "./img/post_card/b_postmark3.png"
+                  alt = "郵戳3"
+                  id = "postmark3" >
                 </div>
               </div>
             </div>`,
