027fe3cf30a823b39d07175d7a8e30268e98efb3
WuMengChe <black.eagle.man@gmail.com>
2020-09-10 11:50:27 +0800

    更新檢測用php

--- php/memberStateCheck.php
+++ php/memberStateCheck.php
@@ -2,9 +2,10 @@
     session_start();
     // $postData = file_get_contents('php://input');
     // $requests = !empty($postData) ? json_decode($postData, true) : array();
-    $user = $_SESSION["memNo"];
-    $userName = $_SESSION["memName"];
-    if($user){
+    
+    if(isset($_SESSION["memNo"])){
+        $user = $_SESSION["memNo"];
+        $userName = $_SESSION["memName"];
         echo $user;
         echo $userName;
     }
