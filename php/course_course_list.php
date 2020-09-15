7400f38305f7f1a63cabcbcaa77e5b72553814cd
PeiYu <peiyuouyang0417@gmail.com>
2020-09-15 19:41:47 +0800

    introduce頁面載入資料庫資料

--- php/course_course_list.php
+++ php/course_course_list.php
@@ -4,13 +4,6 @@
 try {
 
     require_once "connectMySql.php";
-    
-	// $dsn = "mysql:host=localhost;port=8889;dbname=ed102g3;charset=utf8";
-	// $user = "root";
-	// $password = "root";
-	// $options = array(PDO::ATTR_CASE=>PDO::CASE_NATURAL, PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION);
-    // $pdo = new PDO($dsn, $user, $password, $options);
-    
 
 
     $sql = "select a.ski_no , 
