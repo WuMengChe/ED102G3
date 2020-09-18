a9a84b04f0babf5c45efca7d8a717eb4a5312828
ofeliannie <ofeliannie@gmail.com>
2020-09-18 12:46:29 +0800

    後台再推推

--- backstage/backstage_orderDetail.php
+++ backstage/backstage_orderDetail.php
@@ -2,7 +2,7 @@
 try {
     require_once "connectMySql.php";
     // $orderSql = "select mem.MEM_NO, ord.ORD_NO, ord.ORD_DATE, ord.ORD_AMOUNT,ord.ORD_PAY, orddet.ORD_DET_NO ,orddet.ORD_DET_PRICE, ski.SKI_NAME, ski.SKI_IMG, ski.SKI_TEC_NAME, ski.SKI_NO from order_mem ord join member mem on ord.MEM_NO = mem.MEM_NO join order_detial orddet on ord.ORD_NO = orddet.ORD_NO join skill_class ski on ski.SKI_NO = orddet.SKI_NO order by orddet.ORD_DET_NO";
-    $orderDetailSql = "select orddet.*, ski.SKI_NAME, ski.SKI_IMG,ski.SKI_TEC_NAME, ski.SKI_NO from order_detial orddet join skill_class ski on ski.SKI_NO = orddet.SKI_NO";
+    $orderDetailSql = "select orddet.*, ski.SKI_NAME, ski.SKI_IMG,ski.SKI_TEC_NAME, ski.SKI_NO from order_detail orddet join skill_class ski on ski.SKI_NO = orddet.SKI_NO";
 
     // $ordCountSql = "select count(*) ordamount from ORDER_MEM";
     $orderDetail = $pdo->query($orderDetailSql);
