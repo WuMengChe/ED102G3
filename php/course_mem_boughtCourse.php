6055b7b43dd3394e4dcfe0b737c1e7a2072d4b2b
WuMengChe <black.eagle.man@gmail.com>
2020-09-25 15:12:00 +0800

    php小修

--- php/course_mem_boughtCourse.php
+++ php/course_mem_boughtCourse.php
@@ -6,18 +6,23 @@ try {
 
     if ($pdo != false) {
 
-        $sql = "SELECT distinct a.ski_no
-        from order_detail a join order_mem b on a.ord_no = b.ord_no
-        where mem_no = :MEM_NO;";
-        $disAll = $pdo->prepare($sql);
-        $disAll->bindValue(":MEM_NO", $_SESSION["memNo"]);
-        $disAll->execute();
-        if ($disAll->rowCount() == 0) {
+        if(isset($_SESSION["memNo"])){
+            $sql = "SELECT distinct a.ski_no
+            from order_detail a join order_mem b on a.ord_no = b.ord_no
+            where mem_no = :MEM_NO;";
+            $disAll = $pdo->prepare($sql);
+            $disAll->bindValue(":MEM_NO", $_SESSION["memNo"]);
+            $disAll->execute();
+            if ($disAll->rowCount() == 0) {
+                echo 0;
+            } else {
+                $disRow = $disAll->fetchAll(PDO::FETCH_ASSOC);
+                //送出json字串
+                echo json_encode($disRow);
+            }
+        }
+        else{
             echo 0;
-        } else {
-            $disRow = $disAll->fetchAll(PDO::FETCH_ASSOC);
-            //送出json字串
-            echo json_encode($disRow);
         }
 
     }
