4065600fb05777b3d9d6ebec99613d5accd58a8f
Jing-siao <vicky820715@gmail.com>
2020-09-25 22:46:13 +0800

    後臺小修正

--- backstage/backstage_announcement_add.php
+++ backstage/backstage_announcement_add.php
@@ -4,20 +4,18 @@
 try {
 
     require_once "connectMySql.php";
-    if($pdo!=false){
+    if ($pdo != false) {
         $sql = "INSERT INTO ANNOUNCEMENT(ANN_CONTENT,ANN_DATE,ANN_USE) VALUES(:ANN_CONTENT,:ANN_DATE,:ANN_USE);";
 
 
         $disAll = $pdo->prepare($sql);
 
-        $disAll->bindValue(":ANN_CONTENT",$_POST["ANN_CONTENT"]);
-        $disAll->bindValue(":ANN_DATE",$_POST["ANN_DATE"]);
-        $disAll->bindValue(":ANN_USE",$_POST["ANN_USE"]);
+        $disAll->bindValue(":ANN_CONTENT", $_POST["ANN_CONTENT"]);
+        $disAll->bindValue(":ANN_DATE", $_POST["ANN_DATE"]);
+        $disAll->bindValue(":ANN_USE", $_POST["ANN_USE"]);
         $disAll->execute();
-        // header("Location: ". $_redirect);
+        header("Location:./backstage_index.php");
     }
-
-
 } catch (PDOException $e) {
     echo $e->getMessage();
 }
